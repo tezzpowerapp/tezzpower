4fe1bd58de0b4aacc159f012f79d408033022668
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2025-12-26 11:24:31 +0400

    edit bosluq

--- app/(pages)/qr/[slug]/page.jsx
+++ app/(pages)/qr/[slug]/page.jsx
@@ -1,27 +1,19 @@
-// import NotFound from "@/app/(components)/NotFound/NotFound"; // Buna gerek yok
-import { redirect, notFound } from "next/navigation"; // notFound fonksiyonunu kullan
-
+import { redirect, notFound } from "next/navigation";
 export default async function page({ params }) {
   const { slug } = await params;
-
   let data;
-
   try {
     const res = await fetch(`https://api.tezz.az/api/qr-links/${slug}`, {
       cache: "no-store",
     });
-
     if (!res.ok) {
       notFound();
     }
-
     data = await res.json();
   } catch (error) {
     console.error("Error:", error);
-
     notFound();
   }
-
   if (data?.target_url) {
     redirect(data.target_url);
   } else {
