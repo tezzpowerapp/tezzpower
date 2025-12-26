4f72a3175f783fec6ea2cef1a89d2bb385721a5a
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2025-12-26 11:18:07 +0400

    addingss

--- middleware.js
+++ middleware.js
@@ -10,14 +10,11 @@ export function middleware(request) {
         pathname
     } = request.nextUrl;
 
-    // --- DÜZELTME BAŞLANGICI ---
-    // Eğer istek /qr ile başlıyorsa, middleware hiçbir şey yapmasın, direkt geçsin.
+
     if (pathname.startsWith("/qr")) {
         return NextResponse.next();
     }
-    // --- DÜZELTME BİTİŞİ ---
 
-    // 1. /az yolunu URL'den temizle (Redirect)
     if (pathname === `/${defaultLocale}` || pathname.startsWith(`/${defaultLocale}/`)) {
         const newPath = pathname.replace(`/${defaultLocale}`, "");
         return NextResponse.redirect(new URL(newPath || "/", request.url));
@@ -37,7 +34,7 @@ export function middleware(request) {
 
 export const config = {
     matcher: [
-        // api, _next ve static dosyalar hariç her şeyi yakala
+
         '/((?!api|_next/static|_next/image|favicon.ico|.*\\.svg|.*\\.png|.*\\.jpg).*)',
     ],
 };
