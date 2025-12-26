7f48fd35ffabf6499c879e74cc04838a0ebb52fa
Javid Ismayıl <61595060+thejavidev@users.noreply.github.com>
2025-12-26 11:13:57 +0400

    editingsss

--- app/(pages)/qr/[slug]/page.jsx
+++ app/(pages)/qr/[slug]/page.jsx
@@ -2,7 +2,6 @@
 import { redirect, notFound } from "next/navigation"; // notFound fonksiyonunu kullan
 
 export default async function page({ params }) {
-  // Next.js 15 kullanıyorsan params await edilmeli, doğru yapmışsın.
   const { slug } = await params;
 
   let data;
@@ -13,22 +12,19 @@ export default async function page({ params }) {
     });
 
     if (!res.ok) {
-      // API hata verirse direkt 404 sayfasına at
       notFound();
     }
 
     data = await res.json();
   } catch (error) {
     console.error("Error:", error);
-    // Fetch hatası olursa 404 ver
+
     notFound();
   }
 
-  // Veri geldi ve target_url varsa yönlendir
   if (data?.target_url) {
     redirect(data.target_url);
   } else {
-    // Veri geldi ama url yoksa 404
     notFound();
   }
 }
